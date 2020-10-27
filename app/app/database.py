58798ebc64b56c7ba0ed2f99d3085307d10f59cd
wilson romero <futari123@hotmail.com>
2020-10-26 22:03:12 -0500

    primera version 26/10/2020

---
+++ app/app/database.py
@@ -0,0 +1,9 @@
+MYSQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django-shop',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'PORT': '3306'
+    }
+}
